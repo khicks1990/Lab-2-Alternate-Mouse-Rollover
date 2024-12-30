cc845fd00dd2240b0e2ba7db2bc98a5061f0c4bc
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-30 07:37:18 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,47 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.geometry.Pos;
+import javafx.geometry.Insets;
+import javafx.scene.control.Slider;
+import javafx.scene.control.Label;
+import javafx.scene.effect.ColorAdjust;
+import javafx.stage.FileChooser;
+import java.io.File;
+
+/**
+ *  Mouse Rollover
+ */
+
+public class Main extends Application
+{
+   public static void main(String[] args)
+   {
+      // Launch the application.
+      launch(args);
+   }
+
+   @Override
+   public void start(Stage primaryStage)
+   {
+      // Load the images
+
+      // Create an ImageView
+
+      // Register event handlers
+
+
+      // Put everything into a VBox
+
+
+      // Add the main VBox to a scene.
+
+
+      // Set the scene to the stage aand display it.
+      primaryStage.setScene(scene);
+      primaryStage.show();
+   }
+}
